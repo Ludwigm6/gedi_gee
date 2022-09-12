ef29402c076a4645f390823ae653ba9b077a6b55
ziegler_local <ziegler_local@pc19544.AD.Uni-Marburg.DE>
2022-09-12 14:03:49 +0100

    gedi filter included in looper, changed granule list and changed R-script to create granule list.

--- gee_scripts/gedi_looper.js
+++ gee_scripts/gedi_looper.js
@@ -1,15 +1,26 @@
-// input granules // client side list
-// granule format: ["Name", "StartDate", "EndDate"]
 
-print("start: ", new Date())
+
+// INPUT -------------------------------
+
+
+// input granules
+// granule format: ["Name", "Date"]
+
 var granules = [
-['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2020330171806_O11074_02_T08735_02_003_01_V002' , '2020-11-20' , '2020-11-30'],
-['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2020230081607_O09518_02_T01237_02_003_01_V002' , '2020-08-12' , '2020-08-22'],
-['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2019248025112_O04134_02_T00641_02_003_01_V002' , '2019-08-31' , '2019-09-10'],
-['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2020243055419_O09718_03_T07757_02_003_01_V002' , '2020-08-25' , '2020-09-04'],
-['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2021003022816_O11669_02_T07098_02_003_01_V002' , '2020-12-29' , '2021-01-08']
+['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2020330171806_O11074_02_T08735_02_003_01_V002'],
+['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2020230081607_O09518_02_T01237_02_003_01_V002'],
+['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2019248025112_O04134_02_T00641_02_003_01_V002'],
+['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2020243055419_O09718_03_T07757_02_003_01_V002'],
+['LARSE/GEDI/GEDI02_B_002/GEDI02_B_2021003022816_O11669_02_T07098_02_003_01_V002']
                ]
 
+
+//var granule_list = require("users/alicezglr/default/granule_list.js");
+var granules = granule_list.granules
+// load hessen vector: AOI
+var hessen = ee.FeatureCollection("users/alicezglr/hessen");
+
+// FUNCTIONS ---------------------------
 
 // define function for indices
 var S2_SR_indices = function(img){
@@ -36,128 +47,144 @@ var S2_SR_indices = function(img){
     return img.addBands(ndvi).addBands(evi).addBands(ireci);
 }
 
+// define function for Sentinel 1
+var sen1vvvh = function(image){
 
-// define function for cloud mask (based on SCL band)
+  var diff = image.select("VV").subtract(image.select("VH")).rename("VVsubVH")
+  var rati = image.select("VV").divide(image.select("VH")).rename("VVdivVH")
 
-var S2_SR_cloudmask = function (image) {
-  var scl = image.select('SCL');
-  var wantedPixels = scl.gt(3).and(scl.lt(7)).or(scl.eq(1)).or(scl.eq(2));
-  return image.updateMask(wantedPixels)
+  return image.addBands(diff).addBands(rati)
+
+}
+
+//define function to extract date of gedi orbit by name of gedi orbit
+function name2date(namestring){
+  var fulldt = namestring.split("_")[4].substr(0,7);
+  var year = parseInt(fulldt.substr(0,4));
+  var day = parseInt(fulldt.substr(4,7));
+  var date = new Date(year, 0, day+1); //!!! az: I added the +1. JS community does without. But incorrect, right!?!?
+  return(ee.Date(date))
 }
 
 
 
-// load hessen vector
-var hessen = ee.FeatureCollection("users/alicezglr/hessen");
-//var hessen = ee.FeatureCollection("projects/ee-ludwigm6/assets/gedi_hessen/hessen");
-var AOI = hessen.geometry().bounds();
 
+
+// COMPUTE --------------------------
 
 
 // map over granule list
 var results = granules.map(function(g){
 
+
   // Load one GEDI orbit
   var gedi = ee.FeatureCollection(g[0]);
+  //var gedi_date = ee.Date(g[1])
+  var gedi_date = name2date(g[0])
 
 
-
-
-  // Filter a small amount of points in the orbit
-  gedi = gedi.filterBounds(hessen).randomColumn();
-  var gedi_sample = gedi.filter(ee.Filter.lt("random", 0.03));
-    // Buffer the points
-  gedi_sample = gedi_sample.map(function(f){return f.buffer(15)});
-
-    //while testing:
-  //var  gedi_filt = gedi_sample
-
-
-  // Filter gedi points
-  var gedi_filt = gedi
+  gedi = gedi.filterBounds(hessen)
   .filter(ee.Filter.eq("l2b_quality_flag", 1))
   .filter(ee.Filter.gt("sensitivity", 0.9))
   .filter(ee.Filter.neq("pai", null));
-  print("gedi_filt: ", gedi_filt);
-print("Number of shots after filtering: ", gedi_filt.size());
- // Buffer the points
-  gedi_filt = gedi_filt.map(function(f){return f.buffer(12.5)});
-//if number of shots <= 200: ommit! perhaps rather filter in R afterwards: how to do it vs. computation time...
+  /*
+  // Filter a small amount of points in the orbit
+  gedi = gedi.filterBounds(hessen).randomColumn();
+  var gedi_sample = gedi.filter(ee.Filter.lt("random", 0.03));
+  */
+
+  //while testing:
+  var gedi_sample = gedi
+  // Buffer the points and add time as property
+  gedi_sample = gedi_sample.map(function(f){return f.buffer(12.5).set({time: g[1]})})
+
+  print(gedi_sample, "W Time")
+  print("gedi_sample size: ", gedi_sample.size())
+
+    // calc time difference to gedi date
+  var temporalMatch = function(image){
+      var timediff = gedi_date.difference(ee.Date(image.get("system:time_start")), "day").abs()
+      return image.set({timediff: timediff})
+    };
 
 
   // Load Sentinel 2 Surface Reflectance and apply the filtering
   // Uses start and end date specified in the granule list
   var sen2 = ee.ImageCollection('COPERNICUS/S2_SR')
-    .filterBounds(gedi_filt)
-    .filterDate(g[1], g[2])
+    .filterBounds(gedi_sample)
+    .filterDate(gedi_date.advance(-5, "day"), gedi_date.advance(5, "day")) // +- 5 days of gedi orbit
     .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 80)) // less than 80% cloud cover
     .select('B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'SCL')
-    .map(S2_SR_cloudmask)
-    .map(S2_SR_indices) // apply the index calculation function to each image in the ImageCollection
-    .toBands(); // Turn the collection into one multi-band image
+    .map(function(image){
+      var idate = image.date().difference(gedi_date, "day");
+      var dateBand = ee.Image.constant(idate).uint16().rename('S2time')
+      return(image.addBands(dateBand))
+    })
+    .map(S2_SR_indices)
+    .map(temporalMatch)
+    .sort("timediff")
+    .mosaic()
 
-print("sen2: ", sen2)
 
-  var sampledPoints = sen2.sampleRegions({
-  collection: gedi_filt,
+
+
+
+  var sen1 = ee.ImageCollection("COPERNICUS/S1_GRD")
+  .filterBounds(gedi_sample)
+  .filterDate(gedi_date.advance(-3, "day"), gedi_date.advance(3, "day")) // +- 3 days of gedi orbit
+  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', "VV"))
+  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', "VH"))
+  .filter(ee.Filter.eq('instrumentMode', 'IW'))
+  .select("VV", "VH")
+  .map(function(image){
+      var idate = image.date().difference(gedi_date, "day");
+      var dateBand = ee.Image.constant(idate).uint16().rename('S1time')
+      return(image.addBands(dateBand))
+    })
+  .map(sen1vvvh)
+  .map(temporalMatch)
+  .mosaic();
+
+
+
+
+  var stack = sen2.addBands(sen1)
+
+
+
+  var sampledPoints = stack.sampleRegions({
+  collection: gedi_sample,
   scale: 10,
-  properties: ['pai', "beam", "degrade_flag", "l2b_quality_flag",
-  "orbit_number", "sensitivity", "shot_number", "shot_number_within_beam",
-  "solar_azimuth", "solar_elevation"],
+  properties: ['time','pai'],
   geometries: true
-});
-print("sampoi: ", sampledPoints)
-print("size: ", sampledPoints.size());
+})
 
-return(sampledPoints);
-});
 
-// look at output
-print("results: ", results);
 
-/////////////////////////////////
-//remove empty elements form "results"
-/////////////////////////////////
-//function to cast empty entries to 0 and non-empty to 1
-var boolify = function(f){ //input = feature collection
-  var size0 = f.size();
-  //print("size0: ", size0)
-  var booly = ee.Algorithms.If(size0, 1, 0); //an if-statement that should be avoided
-  return(ee.Number(booly));
-};
+  return sampledPoints
 
-//function to add new property (boolean if size !=0) to feature Collection
-var set_bool = function(fc){
-  var booly = boolify(fc);
-  print("fc before: ", fc);
-  fc = fc.set('size0', booly);
-  print("fc after: ", fc);
-  return(fc);
-};
+})
 
-var prop_lst = results.map(set_bool); //filtering not possible with this object
-print("prop_lst: ", prop_lst);
-var prop_ee_lst = ee.List(prop_lst); //cast to ee.List deletes all features in fc but filtering would be possible
-print("prop ee_lst: ", prop_ee_lst);
 
-var prop_ee_filt = prop_ee_lst.filter(ee.Filter.eq("size0", 1));
-print("prop_ee_filt: ", prop_ee_filt);
-var prop_filt = prop_lst.filter(ee.Filter.eq("size0", 1));
-//var prop_filt = prop_lst.filter(ee.Filter.notNull(['item']));
-print("prop_filt: ", prop_filt);
+
+
+print(results, "Sampled Points")
+
+
+
+var resultsCollection = ee.FeatureCollection(results).flatten()
+
+print(resultsCollection.first(), "RES")
 
 
 
 // save each GEDI orbit in a different file
-for(var j in [0,1,2,3,4]){
-  Export.table.toDrive({
-    collection: results[j],
-    description: j,
-    fileNamePrefix: "gedi_"+j,
+
+
+
+Export.table.toDrive({
+    collection: resultsCollection,
+    description: "Res",
+    fileNamePrefix: "gedi_",
     folder: "gedi"
-  });
-}
-
-
-// Hint: I left out the cloud mask for this test since the chosen date is very cloudy and no points are left
-print("end: ", new Date());
+  })
